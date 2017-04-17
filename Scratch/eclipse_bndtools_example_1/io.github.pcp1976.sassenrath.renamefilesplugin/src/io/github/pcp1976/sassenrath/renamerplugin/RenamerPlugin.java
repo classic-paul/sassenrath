89a04d4c90c1db212a3614d0bf9228c3b64960f6
Paul Peacock <pcp1976@gmail>
2017-04-17 11:23:06 +0100

    Added concrete implementation of renamer.

---
+++ Scratch/eclipse_bndtools_example_1/io.github.pcp1976.sassenrath.renamefilesplugin/src/io/github/pcp1976/sassenrath/renamerplugin/RenamerPlugin.java
@@ -0,0 +1,68 @@
+package io.github.pcp1976.sassenrath.renamerplugin;
+
+import java.io.File;
+
+import org.osgi.service.component.annotations.*;
+
+import io.github.pcp1976.sassenrath.api.Plugin;
+import io.github.pcp1976.sassenrath.api.PluginState;
+
+@Component
+public class RenamerPlugin implements Plugin {
+
+	@Override
+	public String getName() {
+		return "File renamer";
+	}
+
+	@Override
+	public void configure() {
+		// TODO write configure
+		throw new UnsupportedOperationException();
+
+	}
+
+	@Override
+	public void enqueue(File file, Plugin plugin) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public PluginState getState() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+		//return null;
+	}
+
+	@Override
+	public void registerClient(Plugin plugin) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+}
