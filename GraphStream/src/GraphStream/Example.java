034fbd049016f095201ea0eeb9379724270828dc
Paul Peacock <pcp1976@gmail>
2017-07-02 22:55:37 +0100

    Removed some comments and stuff.

--- GraphStream/src/GraphStream/Example.java
+++ GraphStream/src/GraphStream/Example.java
@@ -17,9 +17,6 @@ public class Example implements ViewerListener {
 		new Example();
 	}
 	public Example() {
-		// We do as usual to display a graph. This
-		// connect the graph outputs to the viewer.
-		// The viewer is a sink of the graph.
 		Graph graph = new SingleGraph("Clicks");
 		SpriteManager sman = new SpriteManager(graph);
 		
@@ -48,35 +45,13 @@ public class Example implements ViewerListener {
 		graph.addEdge("AB", "A", "B", true);
 		graph.addEdge("BC", "B", "C");
 		graph.addEdge("CA", "A", "C", true);
-		// The default action when closing the view is to quit
-		// the program.
-		//viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
-
-		// We connect back the viewer to the graph,
-		// the graph becomes a sink for the viewer.
-		// We also install us as a viewer listener to
-		// intercept the graphic events.
 		ViewerPipe fromViewer = viewer.newViewerPipe();
 		fromViewer.addViewerListener(this);
 		fromViewer.addSink(graph);
 
-		// Then we need a loop to do our work and to wait for events.
-		// In this loop we will need to call the
-		// pump() method before each use of the graph to copy back events
-		// that have already occurred in the viewer thread inside
-		// our thread.
-
 		while(loop) {
 			fromViewer.pump(); // or fromViewer.blockingPump(); in the nightly builds
-
-			// here your simulation code.
-
-			// You do not necessarily need to use a loop, this is only an example.
-			// as long as you call pump() before using the graph. pump() is non
-			// blocking.  If you only use the loop to look at event, use blockingPump()
-			// to avoid 100% CPU usage. The blockingPump() method is only available from
-			// the nightly builds.
-		}
+			}
 	}
 
 	public void viewClosed(String id) {
